ce709f919299b64c00f48b27d5254d79d9e88bd1
Jacek <jacekpieczatka@hotmail.com>
2019-04-01 21:32:47 +0200

    transform_articles_onpage

--- parsetest.py
+++ parsetest.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
+import pandas as pd
 import unicodedata
 city = "krakow"
 citypl = "Kraków"
@@ -8,10 +9,38 @@ url2parse = "https://gratka.pl/nieruchomosci/mieszkania/" + city + "?page=1"
 r = requests.get(url2parse)
 soup = BeautifulSoup(r.content,"html.parser")
 
-articles = soup.find_all('article')
 
-print(type(articles))
-print(type(soup))
+def transform_articles_onpage():
+
+    teaser_params = soup.find('ul', class_='teaser__params')
+    teaser_price = 'Cena: ' + str(soup.find(attrs={'class' : 'teaser__price'}).contents[0].strip())
+    teaser_anchor = 'Tytuł: ' + soup.find('a', class_='teaser__anchor').contents[0].strip()
+    teaser_location = 'Miasto: ' + soup.find('h3', class_='teaser__location').contents[0].strip().strip(',')
+    teaser_region = 'Województwo: ' + soup.find('span', class_='teaser__region').contents[0]
+    #params_from_teaser = [li.text.strip() for li in teaser_params.find_all('li')]
+    #params_from_teaser.append(teaser_price)
+    #params_from_teaser.append(teaser_anchor)
+    #params_from_teaser.append(teaser_location)
+    #params_from_teaser.append(teaser_region)
+
+    #print(params_from_teaser)
+    print(teaser_params)
+
+
+
+
+
+transform_articles_onpage()
+
+#            inner_ul = soup.find('ul', class_='innerUl')
+ #           inner_items = [li.text.strip() for li in inner_ul.ul.find_all('li')]
+#
+ #           outer_ul_text = soup.ul.span.text.strip()
+  #          inner_ul_text = inner_ul.span.text.strip()
+#
+   #         result = {outer_ul_text: {inner_ul_text: inner_items}}
+    #        print result
+
 
 # get pagination
 def get_max_pages():
