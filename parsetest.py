3d01cbef5c87bac25b5fcfe3d1ca45b83dfd2396
Jacek <jacekpieczatka@hotmail.com>
2019-04-06 21:54:05 +0200

    Working! I can write immos to disk.

--- parsetest.py
+++ parsetest.py
@@ -1,7 +1,8 @@
+import os
+import time
 from bs4 import BeautifulSoup
 import requests
-import pandas as pd
-import unicodedata
+
 city = "krakow"
 citypl = "Kraków"
 url2parse = "https://gratka.pl/nieruchomosci/mieszkania/" + city + "?page=1"
@@ -9,52 +10,50 @@ url2parse = "https://gratka.pl/nieruchomosci/mieszkania/" + city + "?page=1"
 r = requests.get(url2parse)
 soup = BeautifulSoup(r.content,"html.parser")
 
-
-def transform_articles_onpage():
-
-    teaser_params = soup.find('ul', class_='teaser__params')
-    teaser_price = 'Cena: ' + str(soup.find(attrs={'class' : 'teaser__price'}).contents[0].strip())
-    teaser_anchor = 'Tytuł: ' + soup.find('a', class_='teaser__anchor').contents[0].strip()
-    teaser_location = 'Miasto: ' + soup.find('h3', class_='teaser__location').contents[0].strip().strip(',')
-    teaser_region = 'Województwo: ' + soup.find('span', class_='teaser__region').contents[0]
-    #params_from_teaser = [li.text.strip() for li in teaser_params.find_all('li')]
-    #params_from_teaser.append(teaser_price)
-    #params_from_teaser.append(teaser_anchor)
-    #params_from_teaser.append(teaser_location)
-    #params_from_teaser.append(teaser_region)
-
-    #print(params_from_teaser)
-    print(teaser_params)
-
-
-
-
-
-transform_articles_onpage()
-
-#            inner_ul = soup.find('ul', class_='innerUl')
- #           inner_items = [li.text.strip() for li in inner_ul.ul.find_all('li')]
-#
- #           outer_ul_text = soup.ul.span.text.strip()
-  #          inner_ul_text = inner_ul.span.text.strip()
-#
-   #         result = {outer_ul_text: {inner_ul_text: inner_items}}
-    #        print result
-
+mydir = 'D:'
+myfile = 'big_bunch_of_immos'
+target = os.path.join(mydir, myfile)
+f = open(target ,"w+")
 
 # get pagination
 def get_max_pages():
-    max_pages = soup.find("input", {"aria-label":"Numer strony wyników"})['max']
-    return max_pages
+    get_max_pages.max_pages = soup.find("input", {"aria-label":"Numer strony wyników"})['max']
 
-# get_immo_number_onpage
-def count_immos_onpage():
-    count = 0
-    articletags = soup.find_all('article')
-    for tag in articletags:
-        count += 1
-    return count
+#get number of articles on page
+def get_max_articles():
+    get_max_articles.max_articles = len(soup.find_all('article'))
 
+def crawl_for_articles():
+    get_max_pages()
+    get_max_articles()
+
+    for page in range(1, int(get_max_pages.max_pages)):
+
+        url2parse_current = "https://gratka.pl/nieruchomosci/mieszkania/" + city + "?page="+str(page)
+        r = requests.get(url2parse_current)
+        soup = BeautifulSoup(r.content, "html.parser")
+
+        #time.sleep(5)
+
+        for article in range (0, get_max_articles.max_articles):
+
+            teaser_params = soup.find_all('ul', class_='teaser__params')[article]
+            teaser_price = 'Cena: ' + str(soup.find(attrs={'class' : 'teaser__price'}).contents[0].strip())
+            teaser_anchor = 'Tytuł: ' + soup.find('a', class_='teaser__anchor').contents[0].strip()
+            #teaser_location = 'Miasto: ' + soup.find('h3', class_='teaser__location').contents[0].strip().strip(',').strip('\\n')
+            teaser_region = 'Województwo: ' + soup.find('span', class_='teaser__region').contents[0]
+            params_from_teaser = [li.text.strip() for li in teaser_params.find_all('li')]
+            params_from_teaser.append(teaser_price)
+            params_from_teaser.append(teaser_anchor)
+            #params_from_teaser.append(teaser_location)
+            params_from_teaser.append(teaser_region)
+
+            f.write(str(params_from_teaser)+"\n")
+            #print(teaser_params)
+
+crawl_for_articles()
+
+f.close()
 
 #check if primary market
 def check_if_primo():
@@ -73,18 +72,6 @@ def get_district():
         return title[i+1].strip(',')
 
 
-#print(get_max_pages())
-#print(get_district())
-#print(count_immos_onpage())
-#print(check_if_primo())
-#print(all_immos_on_page())
-
-#gotowy loop
-#for request in range(1, int(max_pages)+1):
-#    page = requests.get("https://gratka.pl/nieruchomosci/mieszkania/krakow?page="+max_pages)
-    #get whutcha want here
-    #store it in dynamoDB
-#    print(request)
 
 
 
