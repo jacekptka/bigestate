856c64b94afcaa0b417f300a7a9bad88afcd6323
Jacek <jacekpieczatka@hotmail.com>
2019-05-07 10:29:47 +0200

    zeppelin

--- crawl_gratki_aws.py
+++ crawl_gratki_aws.py
@@ -1,5 +1,6 @@
 import random
 import string
+import re
 
 import boto3
 import os
@@ -38,17 +39,18 @@ def crawl_for_articles():
     crawl_for_articles.estates = ""
     crawl_for_articles.header = ""
 
-    #for page in range(1, int(get_max_pages.max_pages)):
-    for page in range(1, 10):
+    for page in range(1, int(get_max_pages.max_pages)):
+    #for page in range(1, 2):
 
         url2parse_current = "https://gratka.pl/nieruchomosci/mieszkania/" + city + "/sprzedaz?page="+str(page)
         r = requests.get(url2parse_current)
         soup = BeautifulSoup(r.content, "html.parser")
 
         for article in range (0, get_max_articles.max_articles):
+
             teaser_params = soup.find_all('ul', class_='teaser__params')[article]
-            teaser_price = 'Cena: ' + soup.find_all('p', class_='teaser__price')[article].contents[0].strip().replace(" ","")
-            teaser_anchor = 'Tytuł: ' + soup.find_all('a', class_='teaser__anchor')[article].contents[0].strip().replace(",","")
+            teaser_price = 'price:' + soup.find_all('p', class_='teaser__price')[article].contents[0].strip().replace(" ","")
+            teaser_anchor = 'title:' + soup.find_all('a', class_='teaser__anchor')[article].contents[0].strip().replace(",","")
             #teaser_location = 'Miasto: ' + soup.find('h3', class_='teaser__location').contents[0].strip().strip(',').strip('\\n')
             #teaser_region = 'Województwo: ' + soup.find('span', class_='teaser__region').contents[0]
             striped_teaser_params = [li.text.strip() for li in teaser_params.find_all('li')]
@@ -57,54 +59,68 @@ def crawl_for_articles():
             else:
                 params_from_teaser = striped_teaser_params[:3]
 
+            params_from_teaser = [w.replace('Powierzchnia w m2', 'area') for w in params_from_teaser]
+            params_from_teaser = [w.replace('Liczba pokoi', 'no_of_rooms') for w in params_from_teaser]
+            params_from_teaser = [w.replace('Piętro', 'level') for w in params_from_teaser]
+            params_from_teaser = [w.replace('parter', '0') for w in params_from_teaser]
+            params_from_teaser = [re.sub(r'piwnica(.*)', '-1', w) for w in params_from_teaser]
+
             params_from_teaser.append(teaser_price)
             params_from_teaser.append(teaser_anchor)
             #params_from_teaser.append(teaser_location)
             #params_from_teaser.append(teaser_region)
 
             #sprawdź czy rynek pierwotny
-            foundPrimaryMarket = soup.find("span", {"class": "teaser__primaryMarket"})
+            #foundPrimaryMarket = soup.find("span", {"class": "teaser__primaryMarket"})
+            foundPrimaryMarket = soup.find_all("span", {"class": "teaser__primaryMarket"})
             isPrimaryMarket = ""
             if foundPrimaryMarket:
-                isPrimaryMarket += "tak"
+                isPrimaryMarket += "yes"
             else:
-                isPrimaryMarket += "nie"
+                isPrimaryMarket += "no"
 
-            params_from_teaser.append("Rynek Pierwotny: "+isPrimaryMarket)
+            params_from_teaser.append("primary_market:"+isPrimaryMarket)
 
             #dzielnica
             district = ""
-            title = soup.find("a", {"class": "teaser__anchor"})['title'].split(' ')
+            #title = soup.find_all("a", {"class": "teaser__anchor"})['title'].split(' ')
+            title = teaser_anchor.split(' ')
             for i in [i for i, x in enumerate(title) if x == citypl]:
-                if title.index(citypl)+1 < len(title):
+                try:
+                #if title.index(citypl)+1 < len(title):
+
+                    print(str(title.index(citypl)+1) +' __ '+ str(len(title)))
+
                     district = title[i + 1]
                     if district == 'Stare':
                         district = "Stare Miasto"
                     if district == 'Nowa':
                         district = "Nowa Huta"
-                else:
+                    if district == 'Wola':
+                        district = "Wola Justowska"
+                except IndexError:
                     district = "brak"
-                #if title[i + 1] != 0:
-                 #   district = title[i + 1].strip(',')
-                #else:
-                 #   district = "brak"
 
-            params_from_teaser.append("Dzielnica: " + str(district.strip(',')))
+                print(district)
+
+            params_from_teaser.append("district:" + str(district.strip(',')))
 
             keys = []
             values =[]
             for i in params_from_teaser:
 
                 ention = i.split(":")
+
                 keys.append(ention[0])
+
                 values.append(ention[1])
 
-            crawl_for_articles.header = str(keys).replace("'","").replace("[","").replace("]","")+'\n'
+            crawl_for_articles.header = str(keys).replace("'","").replace("[","").replace("]","").replace(" ", "")+'\n'
             estate = str(values)
             temp = estate.replace("'","").replace("[","").replace("]","")
             #crawl_for_articles.body = header
             crawl_for_articles.estates += temp+'\n'
-
+            print(str(page)+str(article))
 
 
 
